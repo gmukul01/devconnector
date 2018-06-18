22627bdb2eb3008d619804cadeff1a4bf4a71255
mukul <mukul@in-mukulb.local>
2018-06-18 07:49:58 +0530

    Added routes for deleting education, experience and profile

--- validation/register.js
+++ validation/register.js
@@ -4,10 +4,10 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  data.name = !isEmpty(data.name) ? data.name.toString() : "";
+  data.email = !isEmpty(data.email) ? data.email.toString() : "";
+  data.password = !isEmpty(data.password) ? data.password.toString() : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2.toString() : "";
 
   if (validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
